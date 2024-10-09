82e6c694e78543ad09e9c6910645c1caa2427b5b
Rodrigo Gandarillas Heredia <rodrigo.gandarillas.heredia@gmail.com>
2024-10-09 12:03:30 -0400

    conexion bd

---
+++ backPedidos/database.js
@@ -0,0 +1,13 @@
+var mysql = require('mysql');
+var dotenv = require('dotenv');
+dotenv.config();
+var connection = mysql.createConnection({
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_DATABASE || 'sistema_pedidos'
+});
+
+
+
+module.exports = connection;
