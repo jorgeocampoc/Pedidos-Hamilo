5024ddc7ca42f4418016d8c0bdd57d8a90fa944b
jorgeocampocerezo <joryorch4000@gmail.com>
2024-10-09 11:28:48 -0400

    proyecto base

---
+++ frontPedidos/hamilo_pedidos/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div>
+    <header>
+
+
+<nav>
+  <RouterLink to="/">Home</RouterLink>
+</nav>
+</header>
+
+<RouterView />
+  </div>
+</template>
+
