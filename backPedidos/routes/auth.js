1ef6774113c977103e1b6c9ca54255cc2959e49b
DaroV4 <darovenegasv@gmail.com>
2024-10-09 17:21:35 -0400

    auth verificado

--- backPedidos/routes/auth.js
+++ backPedidos/routes/auth.js
@@ -9,29 +9,31 @@ router.post('/login', function (req, res, next) {
  
     var query = `SELECT * FROM usuarios WHERE correo = "${correo}";`;
  
-    conexion.query(query, async function (error, results, fields) {
+    conexion.query(query, async function (error, results) {
         if (error) {
             console.log(error);
-            res.status(500).send({
+            res.status(500).json({
                 error: error,
                 message: 'Error al realizar la petición'
             });
         } else {
             if (results.length == 0) {
-                res.status(403).send({
+                res.status(403).json({
                     message: 'El usuario no existe'
                 });
             } else {
                 const usuario = results[0];
+                // console.log(usuario.contrasena);
                 const verifica = await bcrypt.compare(contrasena, usuario.contrasena);
+                // console.log(verifica);
                 if (!verifica) {
-                    res.status(403).send({
+                    res.status(403).json({
                         message: 'Usuario o contraseña incorrectos'
                     });
                 } else {
                     // creamos el token
                     const token = jwt.sign({ usuario: usuario }, 'secretkey', { expiresIn: '1h' });
-                    res.status(200).send({
+                    res.status(200).json({
                         usuario: usuario,
                         token: token
                     });
