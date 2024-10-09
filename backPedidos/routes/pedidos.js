53f1d8c68e1a722568ef6f94db01c7624f2586ab
Rodrigo Gandarillas Heredia <rodrigo.gandarillas.heredia@gmail.com>
2024-10-09 16:36:01 -0400

    cambiando nomenclatura de las rutas

--- backPedidos/routes/pedidos.js
+++ backPedidos/routes/pedidos.js
@@ -23,7 +23,7 @@ router.get("/", function (req, res, next) {
   });
 });
 
-router.post("/", function (req, res, next) {
+router.post("/store", function (req, res, next) {
   const fechaHoy = new Date().toISOString().slice(0, 19).replace("T", " ");
   const { producto_id, cliente_id, vendedor_id, comentario, fecha_entrega, coordenadas, direccion } = req.body;
 
@@ -47,6 +47,77 @@ router.post("/", function (req, res, next) {
   });
 });
 
+/* router.put("/:id", function (req, res, next) {
+  const { comentario, fecha_entrega, coordenadas, direccion } = req.body;
+  const { id } = req.params;
+  const fechaHoy = new Date().toISOString().slice(0, 19).replace("T", " ");
+
+  var query = `UPDATE pedidos SET  "comentario" = "${comentario}","fecha_pedido" = "${fechaHoy}" ,"fecha_entrega" = "${fecha_entrega}", "coordenadas" = "${coordenadas}", "direccion" = "${direccion}" WHERE id = ${id};`;
+
+  // ejecutamos la consulta
+  conexion.query(query, function (error, results, fields) {
+    if (error) {
+      console.log(error);
+      res.status(500).send({
+        error: error,
+        message: "Error al realizar la peticion",
+      });
+    } else {
+      console.log(results.insertId);
+      res.status(200).send({
+        data: results.insertId,
+        message: "Pedido actualizado correctamente",
+      });
+    }
+  });
+
+});
+
+router.delete("/:id", function (req, res, next) {
+  const { id } = req.params;
+  var query = `DELETE FROM pedidos WHERE id = ${id};`;
+
+  // ejecutamos la consulta
+  conexion.query(query, function (error, results, fields) {
+    if (error) {
+      console.log(error);
+      res.status(500).send({
+        error: error,
+        message: "Error al realizar la peticion",
+      });
+    } else {
+      console.log(results.insertId);
+      res.status(200).send({
+        data: results.insertId,
+        message: "Pedido eliminado correctamente",
+      });
+    }
+  });
+}); */
+
+router.put("/estado/:id", function (req, res, next) {
+  const { id } = req.params;
+  const { estado } = req.body;
+  console.log(estado);
+  var query = `UPDATE pedidos SET  estado = "${estado}" WHERE id = ${id};`;
+
+  // ejecutamos la consulta
+  conexion.query(query, function (error, results, fields) {
+    if (error) {
+      console.log(error);
+      res.status(500).send({
+        error: error,
+        message: "Error al realizar la peticion",
+      });
+    } else {
+      console.log(results.insertId);
+      res.status(200).send({
+        data: results.insertId,
+        message: "Estado actualizado correctamente",
+      });
+    }
+  });
+});
 
 
 module.exports = router;
