8966af5ad2cbc286e3eb7318bea3fe83a487bbd6
Rodrigo Gandarillas Heredia <rodrigo.gandarillas.heredia@gmail.com>
2024-10-09 18:58:48 -0400

    soluionando bug duplicado de productos

--- frontPedidos/src/App.vue
+++ frontPedidos/src/App.vue
@@ -6,18 +6,14 @@ const route = useRouter();
 
 <template>
   <div>
-    <header></header>
-    <main class="container mt-5">
-      <RouterView />
-    </main>
-
     <header>
       <nav v-if="route.currentRoute.value.path !== '/login'">
         <!-- <RouterLink to="/login">Login</RouterLink> -->
       </nav>
     </header>
-
-    <RouterView />
+    <main class="container mt-5">
+      <RouterView />
+    </main>
   </div>
 </template>
 
